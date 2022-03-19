6de5513cd059435824febbe6ddf881dc0aa9bbe7
Glenn <a810.glenn@gmail.com>
2022-03-19 22:17:11 +0900

    Chapter 03 연습문제까지

--- var.go
+++ var.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
-
-func main() {
-	var a int = 10
-	var msg string = "변수"
-
-	a = 20
-	msg = "값 변경"
-	fmt.Println(msg, a)
-}
+//import "fmt"
+//
+//func main() {
+//	var a int = 10
+//	var msg string = "변수"
+//
+//	a = 20
+//	msg = "값 변경"
+//	fmt.Println(msg, a)
+//}
