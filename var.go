2c28a14e35c83d05ff96496ca46ec762e6c75569
Glenn <a810.glenn@gmail.com>
2022-03-20 20:52:12 +0900

    ch04 변수 타입변환

--- var.go
+++ var.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 3 // 변수 선언 일반형태
-	var b int     // 초깃값 생략. 초깃값은 타입별 기본값으로 대체
-	var c = 4     // 타입 생략. 변수 타입은 우변 값의 타입이 됨
-	d := 5        // 선언 대입문 :=를 사용해서 var 키워드와 타입 생략
+	a := 3
+	var b float64 = 3.5
 
-	fmt.Println(a, b, c, d)
+	var c int = int(b) // 정수->실수 타입변환하면 소수점 이하 버림 : 3
+	d := float64(a * c)
+
+	var e int64 = 7
+	f := int64(d) * e
+
+	var g int = int(b * 3) // 변환 전 3.5 x 3이 먼저 이루어 진 뒤 변환 : 10
+	var h int = int(b) * 3 // 변환 후 3 x 3으로 연산 : 9
+
+	fmt.Println(g, h, f)
 }
