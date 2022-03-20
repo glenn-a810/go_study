4ca916db5b3057874a95b61743f3266c9167269f
Glenn <a810.glenn@gmail.com>
2022-03-20 20:57:05 +0900

    ch04 타입변경 시 주의점. 큰 범위->작은 범위로 변환 시 값이 변경될 수 있음

--- var.go
+++ var.go
@@ -3,17 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	a := 3
-	var b float64 = 3.5
+	var a int16 = 3456
+	var c = int8(a)
 
-	var c int = int(b) // 정수->실수 타입변환하면 소수점 이하 버림 : 3
-	d := float64(a * c)
-
-	var e int64 = 7
-	f := int64(d) * e
-
-	var g int = int(b * 3) // 변환 전 3.5 x 3이 먼저 이루어 진 뒤 변환 : 10
-	var h int = int(b) * 3 // 변환 후 3 x 3으로 연산 : 9
-
-	fmt.Println(g, h, f)
+	fmt.Println(a)
+	fmt.Println(c)
 }
