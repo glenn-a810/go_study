97054607bdb6d750231ce292f6be9404b35459f1
Glenn <a810.glenn@gmail.com>
2022-03-20 20:41:14 +0900

    ch04 변수선언 다른 방법

--- var.go
+++ var.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var minimumWage int = 10
-	var workingHour int = 20
+	var a int = 3 // 변수 선언 일반형태
+	var b int     // 초깃값 생략. 초깃값은 타입별 기본값으로 대체
+	var c = 4     // 타입 생략. 변수 타입은 우변 값의 타입이 됨
+	d := 5        // 선언 대입문 :=를 사용해서 var 키워드와 타입 생략
 
-	var income int = minimumWage * workingHour
-
-	fmt.Println(minimumWage, workingHour, income)
+	fmt.Println(a, b, c, d)
 }
