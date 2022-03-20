209646a49e9c197e53465a62bed7741ce9efdf21
Glenn <a810.glenn@gmail.com>
2022-03-20 21:01:05 +0900

    ch04 변수 범위

--- var.go
+++ var.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-func main() {
-	var a int16 = 3456
-	var c = int8(a)
+var g int = 10 // 패키지 전역 변수
 
-	fmt.Println(a)
-	fmt.Println(c)
+func main() {
+	var m int = 20 // 지역 변수
+
+	{
+		var s int = 50 // 지역 변수
+		fmt.Println(m, s, g)
+	} // s 지역 변수 사라짐
+
+	fmt.Println(m, s, g) // s가 범위 내 없으므로 에러
 }
