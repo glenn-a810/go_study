593ba4d59265374f1079bb379db026766a82123b
Glenn <a810.glenn@gmail.com>
2022-03-21 22:15:54 +0900

    ch05 표준 입력

--- var.go
+++ var.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	str := "Hello\tGo\t\tWorld\n\"Go\"is Awsome!\n"
+	var a int // 값을 저장할 변수1
+	var b int // 값을 저장할 변수2
 
-	fmt.Print(str)
-	fmt.Printf("%s", str)
-	fmt.Printf("%q", str)
+	n, err := fmt.Scan(&a, &b) // 입력 두 개 받기. &는 변수의 메모리 주소를 입력으로 넘김. 반환값 n은 성공적으로 입력한 값 개수
+	if err != nil {
+		fmt.Println(n, err) // 에러가 발생하면 에러코드 출력
+	} else {
+		fmt.Println(n, a, b) // 정상 입력되면 입력값 출력
+	}
 }
