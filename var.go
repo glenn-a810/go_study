b51cffc113a0cf158f3638a666acd032de2e7ec1
Glenn <a810.glenn@gmail.com>
2022-03-21 22:23:04 +0900

    ch05 한줄 표준 입력

--- var.go
+++ var.go
@@ -7,7 +7,8 @@ func main() {
 	var b int // 값을 저장할 변수2
 
 	//n, err := fmt.Scan(&a, &b) // 입력 두 개 받기. &는 변수의 메모리 주소를 입력으로 넘김. 반환값 n은 성공적으로 입력한 값 개수
-	n, err := fmt.Scanf("%d %d\n", &a, &b) // %d를 %s로 바꿔도 bad verb로 안됨. 변수 선언 자체가 int로 되어있음
+	//n, err := fmt.Scanf("%d %d\n", &a, &b) // %d를 %s로 바꿔도 bad verb로 안됨. 변수 선언 자체가 int로 되어있음
+	n, err := fmt.Scanln(&a, &b) // Scan()과 차이는 마지막 입력값 이후 반드시 enter키로 입력 종료를 해야함
 	if err != nil {
 		fmt.Println(n, err) // 에러가 발생하면 에러코드 출력
 	} else {
