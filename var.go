2db7aa25aad05e7eebbd6b3916dd6c9b231ce5d6
Glenn <a810.glenn@gmail.com>
2022-03-22 21:04:02 +0900

    ch06 << 왼쪽 shift 연산

--- var.go
+++ var.go
@@ -3,13 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var x1 int8 = 34   // 8bit 정수, 00100010
-	var x2 int16 = 34  // 16bit 정수, 00000000 00100010
-	var x3 uint8 = 34  // 8bit 부호가 없는 정수, 00100010
-	var x4 uint16 = 34 // 16bit 부호가 없는 정수, 00000000 00100010
+	var x int8 = 4
+	var y int8 = 64
 
-	fmt.Printf("^%d = %5d,\t %08b\n", x1, ^x1, uint8(^x1))
-	fmt.Printf("^%d = %5d,\t %016b\n", x2, ^x2, uint16(^x2))
-	fmt.Printf("^%d = %5d,\t %08b\n", x3, ^x3, ^x3)
-	fmt.Printf("^%d = %5d,\t %016b\n", x4, ^x4, ^x4)
+	fmt.Printf("%08b x<<2: %08b x<<2: %d\n", x, x<<2, x<<2)
+	fmt.Printf("%08b y<<2: %08b y<<2: %d\n", y, y<<2, y<<2)
 }
