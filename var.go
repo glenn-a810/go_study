e42a301c22b421f920e41a8031b595aad987f706
Glenn <a810.glenn@gmail.com>
2022-03-22 20:51:28 +0900

    ch06 비트 연산

--- var.go
+++ var.go
@@ -3,18 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var x int32 = 7
-	var y int32 = 3
+	var x1 int8 = 34   // 8bit 정수, 00100010
+	var x2 int16 = 34  // 16bit 정수, 00000000 00100010
+	var x3 uint8 = 34  // 8bit 부호가 없는 정수, 00100010
+	var x4 uint16 = 34 // 16bit 부호가 없는 정수, 00000000 00100010
 
-	var s float32 = 3.14
-	var t float32 = 5
-
-	fmt.Println("x + y = ", x+y)
-	fmt.Println("x - y = ", x-y)
-	fmt.Println("x * y = ", x*y)
-	fmt.Println("x / y = ", x/y)
-	fmt.Println("x % y = ", x%y)
-
-	fmt.Println("s * t = ", s*t)
-	fmt.Println("s / t = ", s/t)
+	fmt.Printf("^%d = %5d,\t %08b\n", x1, ^x1, uint8(^x1))
+	fmt.Printf("^%d = %5d,\t %016b\n", x2, ^x2, uint16(^x2))
+	fmt.Printf("^%d = %5d,\t %08b\n", x3, ^x3, ^x3)
+	fmt.Printf("^%d = %5d,\t %016b\n", x4, ^x4, ^x4)
 }
