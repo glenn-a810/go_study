52360abbc224f686d896a56e6646ce127aeb9626
Glenn <a810.glenn@gmail.com>
2022-03-23 11:23:32 +0900

    ch07 재귀호출

--- var.go
+++ var.go
@@ -2,23 +2,15 @@ package main
 
 import "fmt"
 
-func Divide(a, b int) (result int, success bool) {
-	if b == 0 {
-		//return 0, false
-		result = 0
-		success = false
-		return
-	} else {
-		//return a / b, true
-		result = a / b
-		success = true
+func printNo(n int) {
+	if n == 0 {
 		return
 	}
+	fmt.Println(n)
+	printNo(n - 1)
+	fmt.Println("After", n)
 }
 
 func main() {
-	c, success := Divide(9, 3)
-	fmt.Println(c, success)
-	d, success := Divide(9, 0)
-	fmt.Println(d, success)
+	printNo(3)
 }
