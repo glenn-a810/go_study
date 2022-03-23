3d501fbf06022943c63dd2ed7a45885b13c2916c
Glenn <a810.glenn@gmail.com>
2022-03-23 10:58:55 +0900

    ch07 반복코드를 함수를 써서 효율적인 코드로 바꾸기

--- var.go
+++ var.go
@@ -2,19 +2,29 @@ package main
 
 import "fmt"
 
+func PrintAvgScore(name string, math int, eng int, history int) {
+	total := math + eng + history
+	avg := total / 3
+	fmt.Println(name, "님 평균 점수는", avg, "입니다")
+}
+
 func main() {
-	math := 80
-	eng := 74
-	history := 95
-	fmt.Println("루이의 평균 점수는", (math+eng+history)/3, "입니다")
+	//math := 80
+	//eng := 74
+	//history := 95
+	//fmt.Println("루이의 평균 점수는", (math+eng+history)/3, "입니다")
+	//
+	//math = 88
+	//eng = 92
+	//history = 53
+	//fmt.Println("오드의 평균 점수는", (math+eng+history)/3, "입니다")
+	//
+	//math = 78
+	//eng = 73
+	//history = 78
+	//fmt.Println("하루의 평균 점수는", (math+eng+history)/3, "입니다")
 
-	math = 88
-	eng = 92
-	history = 53
-	fmt.Println("오드의 평균 점수는", (math+eng+history)/3, "입니다")
-
-	math = 78
-	eng = 73
-	history = 78
-	fmt.Println("하루의 평균 점수는", (math+eng+history)/3, "입니다")
+	PrintAvgScore("루이", 80, 74, 95)
+	PrintAvgScore("오드", 88, 92, 53)
+	PrintAvgScore("하루", 78, 73, 78)
 }
