261365636ce8a62f66ad9f3465ab0f53996fbb6d
Glenn <a810.glenn@gmail.com>
2022-03-25 16:22:04 +0900

    ch09 중첩 if 사용

--- var.go
+++ var.go
@@ -2,32 +2,20 @@ package main
 
 import "fmt"
 
-//var price int
-
-func HasRichFriend() bool {
-	return true
-}
-
-func FriendCount() int {
-	return 3
+func getMyAge() (int, bool) {
+	return 33, true
 }
 
 func main() {
-	price := 35000
-
-	if price > 50000 {
-		if HasRichFriend() {
-			fmt.Println("신발끈을 묶는다")
-		} else {
-			fmt.Println("돈을 나눠낸다")
-		}
-	} else if price >= 30000 && price <= 50000 {
-		if FriendCount() > 3 {
-			fmt.Println("신발끈을 묶는다")
-		} else if FriendCount() <= 3 {
-			fmt.Println("돈을 나눠낸다")
-		}
-	} else if price < 30000 {
-		fmt.Println("내가 낸다")
+	if age, ok := getMyAge(); ok && age < 20 {
+		fmt.Println("어림", age)
+	} else if ok && age < 30 {
+		fmt.Println("늙는중", age)
+	} else if ok {
+		fmt.Println("틀", age)
+	} else {
+		fmt.Println("Error")
 	}
+
+	fmt.Println("나이는 : ", age) // if 초기문에서 선언한 변수는 if문 안에서만 사용가능
 }
