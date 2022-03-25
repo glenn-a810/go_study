6e3e3d4325b271afba0c444f0d26726ac8773bb9
Glenn <a810.glenn@gmail.com>
2022-03-25 15:50:14 +0900

    ch09 else if문 사용

--- var.go
+++ var.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	light := "red"
+	temp := 33
 
-	if light == "green" {
-		fmt.Println("길을 건넌다")
+	if temp > 28 {
+		fmt.Println("에어컨을 켠다")
+	} else if temp <= 3 {
+		fmt.Println("히터를 켠다")
 	} else {
 		fmt.Println("대기한다")
 	}
