6c624e293fc92b149f7e5236f621e503b2565a0e
Glenn <a810.glenn@gmail.com>
2022-03-25 16:29:05 +0900

    ch09 연습문제

--- var.go
+++ var.go
@@ -2,20 +2,23 @@ package main
 
 import "fmt"
 
-func getMyAge() (int, bool) {
-	return 33, true
-}
-
 func main() {
-	if age, ok := getMyAge(); ok && age < 20 {
-		fmt.Println("어림", age)
-	} else if ok && age < 30 {
-		fmt.Println("늙는중", age)
-	} else if ok {
-		fmt.Println("틀", age)
-	} else {
-		fmt.Println("Error")
-	}
+	temp := 30
+	rain := 40
 
-	fmt.Println("나이는 : ", age) // if 초기문에서 선언한 변수는 if문 안에서만 사용가능
+	if temp >= 25 {
+		if rain >= 80 {
+			fmt.Println("덥고 비가 옵니다")
+		} else if rain >= 20 {
+			fmt.Println("덥고 습합니다")
+		} else {
+			fmt.Println("야외 활동하기 좋습니다")
+		}
+	} else {
+		if temp < 10 || rain >= 80 {
+			fmt.Println("야외 활동하기 좋지 않습니다")
+		} else {
+			fmt.Println("좋은 날씨입니다")
+		}
+	}
 }
