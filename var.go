26e3ef9b8f5bc80e9ad9cf6bf4363dc889b23c39
Glenn <a810.glenn@gmail.com>
2022-03-25 15:56:16 +0900

    ch09 논리연산으로 if 사용

--- var.go
+++ var.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	temp := 33
+	var age = 22
 
-	if temp > 28 {
-		fmt.Println("에어컨을 켠다")
-	} else if temp <= 3 {
-		fmt.Println("히터를 켠다")
+	if age >= 10 && age <= 15 {
+		fmt.Println("어리구만")
+	} else if age > 30 || age < 20 {
+		fmt.Println("20대는 아니네")
 	} else {
-		fmt.Println("대기한다")
+		fmt.Println("늘금")
 	}
 }
