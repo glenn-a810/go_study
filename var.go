307fcf01b1d365a6352da6a428350956e285c4ee
Glenn <a810.glenn@gmail.com>
2022-03-26 15:21:58 +0900

    ch10 switch 사용

--- var.go
+++ var.go
@@ -3,22 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	temp := 30
-	rain := 40
+	a := 3
 
-	if temp >= 25 {
-		if rain >= 80 {
-			fmt.Println("덥고 비가 옵니다")
-		} else if rain >= 20 {
-			fmt.Println("덥고 습합니다")
-		} else {
-			fmt.Println("야외 활동하기 좋습니다")
-		}
-	} else {
-		if temp < 10 || rain >= 80 {
-			fmt.Println("야외 활동하기 좋지 않습니다")
-		} else {
-			fmt.Println("좋은 날씨입니다")
-		}
+	switch a {
+	case 1:
+		fmt.Println("a == 1")
+	case 2:
+		fmt.Println("a == 2")
+	case 3:
+		fmt.Println("a == 3")
+	case 4:
+		fmt.Println("a == 4")
+	default:
+		fmt.Println("a > 4")
 	}
 }
