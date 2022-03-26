d095f82e48a85c720a2c8ea6cb27dd30e4e4ef4e
Glenn <a810.glenn@gmail.com>
2022-03-26 16:12:58 +0900

    ch10 break와 fallthrough

--- var.go
+++ var.go
@@ -2,35 +2,25 @@ package main
 
 import "fmt"
 
-type ColorType int // 별칭 ColorType을 선언하고 const 열거값 정의
-
-const (
-	Red ColorType = iota
-	Blue
-	Green
-	Yellow
-)
-
-// 각 ColorType 열거값에 따른 문자열 반환
-func colorToString(color ColorType) string {
-	switch color {
-	case Red:
-		return "Red"
-	case Blue:
-		return "Blue"
-	case Green:
-		return "Green"
-	case Yellow:
-		return "Yellow"
-	default:
-		return "Undefined"
-	}
-}
-
-func getMyFavoriteColor() ColorType {
-	return Blue
-}
-
 func main() {
-	fmt.Println("My favorite color is", colorToString(getMyFavoriteColor()))
+	a := 3
+
+	switch a {
+	case 1:
+		fmt.Println("a == 1")
+		break
+	case 2:
+		fmt.Println("a == 2")
+		break
+	case 3:
+		fmt.Println("a == 3")
+		fallthrough
+	case 4:
+		fmt.Println("a == 4")
+		fallthrough
+	case 5:
+		fmt.Println("a == 5")
+	default:
+		fmt.Println("a > 5")
+	}
 }
