fead57b8040d0b39add41a96a13c4bfd7b2c7584
Glenn <a810.glenn@gmail.com>
2022-03-26 15:27:19 +0900

    ch10 switch 사용

--- var.go
+++ var.go
@@ -3,18 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	a := 3
+	day := 3
 
-	switch a {
+	switch day {
 	case 1:
-		fmt.Println("a == 1")
+		fmt.Println("첫째 날입니다.")
+		fmt.Println("오늘은 팀미팅이 있습니다.")
 	case 2:
-		fmt.Println("a == 2")
+		fmt.Println("둘째 날입니다.")
+		fmt.Println("오늘은 면접이 있습니다.")
 	case 3:
-		fmt.Println("a == 3")
+		fmt.Println("셋째 날입니다.")
+		fmt.Println("설계안을 확정하는 날입니다.")
 	case 4:
-		fmt.Println("a == 4")
+		fmt.Println("넷째 날입니다.")
+		fmt.Println("예산을 확정하는 날입니다.")
+	case 5:
+		fmt.Println("다섯째 날입니다.")
+		fmt.Println("최종 계약하는 날입니다.")
 	default:
-		fmt.Println("a > 4")
+		fmt.Println("프로젝트를 진행하세요.")
 	}
 }
