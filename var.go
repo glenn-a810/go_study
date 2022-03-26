68a64a9813efdd226521b1afe861f073956fc5dd
Glenn <a810.glenn@gmail.com>
2022-03-26 15:39:11 +0900

    ch10 조건문 비교

--- var.go
+++ var.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	day := "thursday"
+	temp := 18
 
-	switch day {
-	case "monday", "tuesday":
-		fmt.Println("월,화요일은 수업 가는 날입니다.")
-	case "wednesday", "thursday", "friday":
-		fmt.Println("수,목,금요일은 실습 가는 날입니다.")
+	switch true {
+	case temp < 10, temp > 30:
+		fmt.Println("바깥 활동하기 좋은 날씨가 아닙니다.")
+	case temp >= 10 && temp < 20:
+		fmt.Println("약간 추울 수 있으니 가벼운 겉옷을 준비하세요.")
+		// 이미 두번째 case를 실행했기 때문에 아래는 검사하지 않음
+	case temp >= 15 && temp < 25:
+		fmt.Println("야외 활동하기 좋은 날씨입니다.")
+	default:
+		fmt.Println("따뜻합니다.")
 	}
 }
