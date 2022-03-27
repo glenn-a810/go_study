391eb1e6c2e076628812605de3c439d2bdb5a2ab
Glenn <a810.glenn@gmail.com>
2022-03-27 22:00:18 +0900

    ch11 중첩 for문

--- var.go
+++ var.go
@@ -1,28 +1,12 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	for true { // 무한루프
-		fmt.Println("입력하세요")
-		var number int
-		_, err := fmt.Scanln(&number) // 한 줄 입력받음
-		if err != nil {               // 숫자가 아닌 경우
-			fmt.Println("숫자를 입력하세요")
-
-			// 키보드 버퍼를 삭제
-			stdin.ReadString('\n')
-			continue // 무한루프로 돌아감
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 5; j++ {
+			fmt.Print("*")
 		}
-		fmt.Printf("입력하신 숫자는 %d입니다.\n", number)
-		if number%2 == 0 { // 짝수 검사
-			break // for문을 종료
-		}
+		fmt.Println()
 	}
-	fmt.Println("for문이 종료되었습니다.")
 }
