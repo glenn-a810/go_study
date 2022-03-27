12e34fac7423b5cb2216644e437defb2c6e95229
Glenn <a810.glenn@gmail.com>
2022-03-27 22:16:56 +0900

    ch11 for문에 레이블 사용

--- var.go
+++ var.go
@@ -3,22 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	dan := 2
+	a := 1
 	b := 1
 
-	for true {
-		for true {
-			fmt.Printf("%d * %d = %d\n", dan, b, dan*b)
-			b++
-			if b == 10 {
-				break
+OuterFor: // 레이블 정의
+	for ; a <= 9; a++ {
+		for b = 1; b <= 9; b++ {
+			if a*b == 45 {
+				break OuterFor // 레이블에 가장 먼저 포함된 for문까지 종료
 			}
 		}
-		b = 1
-		dan++
-		if dan == 10 {
-			break
-		}
 	}
-	fmt.Println("for문이 종료되었습니다.")
+	fmt.Printf("%d * %d = %d\n", a, b, a*b)
 }
