98dd0168bec7f472b08dd03c1410970d9af821fe
Glenn <a810.glenn@gmail.com>
2022-03-27 22:40:43 +0900

    ch11 연습문제. 내가 푼거

--- var.go
+++ var.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	for i := 2; i < 10; i++ {
-		if i >= 3 && i <= 6 {
+	for i := 1; i < 10; i++ {
+		if i%2 == 0 {
 			continue
 		}
 		for j := 1; j < 10; j++ {
-			fmt.Println(i, "*", j, "=", i*j)
+			if i != j {
+				continue
+			}
+			fmt.Printf("%d * %d = %d\n", i, j, i*j)
 		}
-		fmt.Println()
 	}
 }
