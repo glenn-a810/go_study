6ee84482fddd51efd7f4f8a3eb63abdd7e91a1f4
Glenn <a810.glenn@gmail.com>
2022-03-27 22:07:35 +0900

    ch11 중첩 for문

--- var.go
+++ var.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 5; j++ {
-			fmt.Print("*")
+	dan := 2
+	b := 1
+
+	for true {
+		for true {
+			fmt.Printf("%d * %d = %d\n", dan, b, dan*b)
+			b++
+			if b == 10 {
+				break
+			}
 		}
-		fmt.Println()
+		b = 1
+		dan++
+		if dan == 10 {
+			break
+		}
 	}
+	fmt.Println("for문이 종료되었습니다.")
 }
