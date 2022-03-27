16ba7e4082769dcf80b8299efcc4d688e7d75e43
Glenn <a810.glenn@gmail.com>
2022-03-27 22:21:47 +0900

    ch11 중첩 for와 레이블이 없는 반복문

--- var.go
+++ var.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
+func find45(a int) (int, bool) { // 곱해서 45가 되는 값 찾기
+	for b := 1; b <= 9; b++ {
+		if a*b == 45 {
+			return b, true
+		}
+	}
+	return 0, false
+}
+
 func main() {
 	a := 1
-	b := 1
+	b := 0
 
-OuterFor: // 레이블 정의
 	for ; a <= 9; a++ {
-		for b = 1; b <= 9; b++ {
-			if a*b == 45 {
-				break OuterFor // 레이블에 가장 먼저 포함된 for문까지 종료
-			}
+		var found bool
+		if b, found = find45(a); found {
+			break
 		}
 	}
 	fmt.Printf("%d * %d = %d\n", a, b, a*b)
