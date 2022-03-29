560e61ae20bfbbf5310c78ff039521da9c0474b6
Glenn <a810.glenn@gmail.com>
2022-03-29 21:26:51 +0900

    ch13 구조체 값 복사.

--- var.go
+++ var.go
@@ -2,33 +2,20 @@ package main
 
 import "fmt"
 
-type User struct {
-	Name  string
-	ID    string
+type Student struct {
 	Age   int
-	Level int // User의 Level 필드
+	No    int
+	Score float64
 }
 
-type VIPUser struct {
-	User
-	Price int
-	Level int // VIPUser의 Level 필드
+func PrintStudent(s Student) {
+	fmt.Printf("나이:%d 번호:%d 점수:%.2f\n", s.Age, s.No, s.Score)
 }
 
 func main() {
-	user := User{"오드", "odd", 11, 10}
-	vip := VIPUser{
-		User{"루이", "luwie", 12, 20},
-		250,
-		3,
-	}
+	var student = Student{15, 23, 88.2}
 
-	fmt.Printf("유저: %s ID: %s 나이: %d\n", user.Name, user.ID, user.Age)
-	fmt.Printf("VIP 유저: %s ID: %s 나이: %d VIP 레벨: %d 유저레벨: %d\n",
-		vip.Name,
-		vip.ID,
-		vip.Age,
-		vip.Level,      // VIPUser의 Level
-		vip.User.Level, // 포함된 구조체명을 쓰고 접근
-	)
+	student2 := student
+
+	PrintStudent(student2)
 }
