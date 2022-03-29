b0c5d3eefbf490a07a8751eecee167dc3bc27cc8
Glenn <a810.glenn@gmail.com>
2022-03-29 21:19:51 +0900

    ch13 구조체 포함된 필드 중복해결

--- var.go
+++ var.go
@@ -3,31 +3,32 @@ package main
 import "fmt"
 
 type User struct {
-	Name string
-	ID   string
-	Age  int
+	Name  string
+	ID    string
+	Age   int
+	Level int // User의 Level 필드
 }
 
 type VIPUser struct {
 	User
-	VIPLevel int
-	Price    int
+	Price int
+	Level int // VIPUser의 Level 필드
 }
 
 func main() {
-	user := User{"오드", "odd", 11}
+	user := User{"오드", "odd", 11, 10}
 	vip := VIPUser{
-		User{"루이", "luwie", 12},
-		3,
+		User{"루이", "luwie", 12, 20},
 		250,
+		3,
 	}
 
 	fmt.Printf("유저: %s ID: %s 나이: %d\n", user.Name, user.ID, user.Age)
-	fmt.Printf("VIP 유저: %s ID: %s 나이: %d VIP 레벨: %d VIP 가격: %d만원\n",
+	fmt.Printf("VIP 유저: %s ID: %s 나이: %d VIP 레벨: %d 유저레벨: %d\n",
 		vip.Name,
 		vip.ID,
 		vip.Age,
-		vip.VIPLevel,
-		vip.Price,
+		vip.Level,      // VIPUser의 Level
+		vip.User.Level, // 포함된 구조체명을 쓰고 접근
 	)
 }
