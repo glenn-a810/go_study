9787b24a8d991ecc63022a4808296b2b06a626fa
Glenn <a810.glenn@gmail.com>
2022-03-29 21:36:36 +0900

    ch13 메모리 패딩을 고려한 필드 배치

--- var.go
+++ var.go
@@ -6,11 +6,14 @@ import (
 )
 
 type User struct {
-	Age   int32   // 4바이트
-	Score float64 // 8바이트
+	A int8 // 1바이트
+	C int8 // 1바이트
+	E int8 // 1바이트
+	B int  // 8바이트
+	D int  // 8바이트
 }
 
 func main() {
-	user := User{23, 77.2}
-	fmt.Println(unsafe.Sizeof(user)) // 해당 변수의 메모리 공간 크기 반환
+	user := User{1, 2, 3, 4, 5}
+	fmt.Println(unsafe.Sizeof(user))
 }
