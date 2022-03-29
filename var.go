a2f18a06446a282e9ae86e03a259468eab862791
Glenn <a810.glenn@gmail.com>
2022-03-29 21:03:00 +0900

    ch13 구조체 기본 사용

--- var.go
+++ var.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
-	a := [2][5]int{
-		{1, 2, 3, 4, 5},
-		{5, 6, 7, 8, 9},
-	}
+type House struct { // 구조체 정의
+	Address string
+	Size    int
+	Price   float64
+	Type    string
+}
 
-	for _, arr := range a {
-		for _, v := range arr {
-			fmt.Print(v, " ")
-		}
-		fmt.Println()
-	}
+func main() {
+	var house House              // 구조체 변수 선언
+	house.Address = "우주정거장 A810" // 각 필드값 초기화
+	house.Size = 28
+	house.Price = 3.5
+	house.Type = "오피스텔"
+
+	fmt.Println("주소 : ", house.Address)
+	fmt.Printf("크기 : %d평\n", house.Size)
+	fmt.Printf("가격 : %.2f억원\n", house.Price)
+	fmt.Println("타입 : ", house.Type)
 }
