b9a9ffe5edd2507fae7931a53ff3a02a8bb81990
Glenn <a810.glenn@gmail.com>
2022-03-30 20:00:43 +0900

    ch14 포인터를 쓰는 경우, ChangeData()에서 메모리 주소는 8바이트 숫자값이므로 8바이트만 복사됨

--- var.go
+++ var.go
@@ -7,15 +7,23 @@ type Data struct {
 	data  [200]int
 }
 
-func ChangeData(arg Data) { // 매개변수로 Data를 받음
-	arg.value = 999     // arg 데이터를 변경
-	arg.data[100] = 999 // arg 데이터를 변경
+//func ChangeData(arg Data) { // 매개변수로 Data를 받음
+//	arg.value = 999     // arg 데이터를 변경
+//	arg.data[100] = 999 // arg 데이터를 변경
+//}
+
+func ChangeData(arg *Data) { // 매개변수로 Data 포인터를 받음
+	//arg.value = 999
+	//arg.data[100] = 999
+	(*arg).value = 999 // arg.value로도 되지만 원래 포인터 변수는 이렇게 써야함
+	(*arg).data[100] = 999
 }
 
 func main() {
 	var data Data
 
-	ChangeData(data) // 인수로 data를 넣음
+	//ChangeData(data) // 인수로 data를 넣음
+	ChangeData(&data) // 인수로 data의 주소를 넘김
 	fmt.Printf("value = %d\n", data.value)
 	fmt.Printf("data[100] = %d\n", data.data[100])
 }
