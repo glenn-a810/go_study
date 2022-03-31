ec6c95f0c7a90e70bab66e856d3618e3174105ad
Glenn <a810.glenn@gmail.com>
2022-03-31 22:05:20 +0900

    ch15 문자열 합산

--- var.go
+++ var.go
@@ -7,12 +7,19 @@ import (
 )
 
 func main() {
-	var str string = "Hello World"
-	var slice []byte = []byte(str)
+	var str string = "Hello"
+	stringHeader := (*reflect.StringHeader)(unsafe.Pointer(&str)) // 구조체로 변경
+	addr1 := stringHeader.Data                                    // Data 필드값을 addr1 변수로 저장
 
-	stringHeader := (*reflect.StringHeader)(unsafe.Pointer(&str))
-	sliceHeader := (*reflect.StringHeader)(unsafe.Pointer(&slice))
+	str += " World"            // 문자열 추가
+	addr2 := stringHeader.Data // Data 필드값을 addr2 변수로 저장
 
-	fmt.Printf("str:\t%x\n", stringHeader.Data)
-	fmt.Printf("slice:\t%x\n", sliceHeader.Data)
+	str += " Welcome!"         // 또 문자열 추가
+	addr3 := stringHeader.Data // Data 필드값을 addr3 변수로 저장
+
+	// 메모리 주소값 출력
+	fmt.Println(str)
+	fmt.Printf("addr1:\t%x\n", addr1)
+	fmt.Printf("addr2:\t%x\n", addr2)
+	fmt.Printf("addr3:\t%x\n", addr3)
 }
