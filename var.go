1ea9ba3c2d0c47505ab1787cc1b078edc660ab57
Glenn <a810.glenn@gmail.com>
2022-03-31 21:42:54 +0900

    ch15 문자열 구조

--- var.go
+++ var.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	str1 := "BBB"
-	str2 := "aaaaAAA"
-	str3 := "BBAD"
-	str4 := "ZZZ"
+	str1 := "안녕하세요. 한글 문자열입니다."
+	str2 := str1
 
-	fmt.Printf("%s > %s : %v\n", str1, str2, str1 > str2)
-	fmt.Printf("%s < %s : %v\n", str1, str3, str1 < str3)
-	fmt.Printf("%s <= %s : %v\n", str1, str4, str1 <= str4)
+	fmt.Printf(str1)
+	fmt.Printf("\n")
+	fmt.Printf(str2)
 }
