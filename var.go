3cd4a3f0f4c689d6a8916e9ad96ae4c6d3a39d33
Glenn <a810.glenn@gmail.com>
2022-03-31 20:11:58 +0900

    ch15 문자열 합치기

--- var.go
+++ var.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	str := "Hello 월드!"
+	str1 := "Hello"
+	str2 := "World"
 
-	for _, v := range str {
-		fmt.Printf("타입:%T 값:%d 문자:%c\n", v, v, v)
-	}
+	str3 := str1 + " " + str2
+	fmt.Println(str3)
+
+	str1 += " " + str2
+	fmt.Println(str1)
 }
