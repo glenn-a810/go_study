f5c8081476837731ac6c29c283fba576802d4d80
Glenn <a810.glenn@gmail.com>
2022-03-31 20:09:26 +0900

    ch15 range 키워드를 이용해 한 글자씩 순회하기

--- var.go
+++ var.go
@@ -4,9 +4,8 @@ import "fmt"
 
 func main() {
 	str := "Hello 월드!"
-	arr := []rune(str)
 
-	for i := 0; i < len(str); i++ {
-		fmt.Printf("타입:%T 값:%d 문자값:%c\n", arr[i], arr[i], arr[i])
+	for _, v := range str {
+		fmt.Printf("타입:%T 값:%d 문자:%c\n", v, v, v)
 	}
 }
