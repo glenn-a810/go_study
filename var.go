344412f057cb952e718b7827e151e97fb4b8909c
Glenn <a810.glenn@gmail.com>
2022-03-31 22:18:57 +0900

    ch15 문자열 합산시 메모리 절약

--- var.go
+++ var.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"unsafe"
+	"strings"
 )
 
+func ToUpper1(str string) string {
+	var rst string
+	for _, c := range str {
+		if c >= 'a' && c <= 'z' {
+			rst += string('A' + (c - 'a')) // 합연산
+		} else {
+			rst += string(c)
+		}
+	}
+	return rst
+}
+
+func ToUpper2(str string) string {
+	var builder strings.Builder
+	for _, c := range str {
+		if c >= 'a' && c <= 'z' {
+			builder.WriteRune('A' + (c - 'a')) // strings.Builder 사용
+		} else {
+			builder.WriteRune(c)
+		}
+	}
+	return builder.String()
+}
+
 func main() {
-	var str string = "Hello"
-	stringHeader := (*reflect.StringHeader)(unsafe.Pointer(&str)) // 구조체로 변경
-	addr1 := stringHeader.Data                                    // Data 필드값을 addr1 변수로 저장
+	var str string = "Hello World"
 
-	str += " World"            // 문자열 추가
-	addr2 := stringHeader.Data // Data 필드값을 addr2 변수로 저장
-
-	str += " Welcome!"         // 또 문자열 추가
-	addr3 := stringHeader.Data // Data 필드값을 addr3 변수로 저장
-
-	// 메모리 주소값 출력
-	fmt.Println(str)
-	fmt.Printf("addr1:\t%x\n", addr1)
-	fmt.Printf("addr2:\t%x\n", addr2)
-	fmt.Printf("addr3:\t%x\n", addr3)
+	fmt.Println(ToUpper1(str))
+	fmt.Println(ToUpper2(str))
 }
