ba509485cd72b7f9dcc832bb1150054e2d57cd4c
Glenn <a810.glenn@gmail.com>
2022-03-31 20:07:24 +0900

    ch15 []rune으로 타입 변환 후 한 글자씩 순회하기

--- var.go
+++ var.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func main() {
 	str := "Hello 월드!"
+	arr := []rune(str)
 
 	for i := 0; i < len(str); i++ {
-		fmt.Printf("타입:%T 값:%d 문자값:%c\n", str[i], str[i], str[i])
+		fmt.Printf("타입:%T 값:%d 문자값:%c\n", arr[i], arr[i], arr[i])
 	}
 }
