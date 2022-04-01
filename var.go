ca2ab19e5a4438de8c4fe960c02d0eea21d1a2de
Glenn <a810.glenn@gmail.com>
2022-04-01 19:48:44 +0900

    ch16 패키지 사용

--- var.go
+++ var.go
@@ -2,36 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math/rand"
 )
 
-func ToUpper1(str string) string {
-	var rst string
-	for _, c := range str {
-		if c >= 'a' && c <= 'z' {
-			rst += string('A' + (c - 'a')) // 합연산
-		} else {
-			rst += string(c)
-		}
-	}
-	return rst
-}
-
-func ToUpper2(str string) string {
-	var builder strings.Builder
-	for _, c := range str {
-		if c >= 'a' && c <= 'z' {
-			builder.WriteRune('A' + (c - 'a')) // strings.Builder 사용
-		} else {
-			builder.WriteRune(c)
-		}
-	}
-	return builder.String()
-}
-
 func main() {
-	var str string = "Hello World"
-
-	fmt.Println(ToUpper1(str))
-	fmt.Println(ToUpper2(str))
+	fmt.Println(rand.Int())
 }
