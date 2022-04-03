e76cd4c7c10c2bc598da50121247efcb0d78f294
Glenn <a810.glenn@gmail.com>
2022-04-03 20:01:35 +0900

    ch17 랜덤한 숫자 생성하기 (Time을 Seed로)

--- var.go
+++ var.go
@@ -3,18 +3,12 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println(rand.Int())
+	rand.Seed(time.Now().UnixNano()) // 시간 값을 랜덤시드로 설정
+
+	n := rand.Intn(100)
+	fmt.Println(n)
 }
-
-//import {
-//	"text/template" // template 패키지
-//	htemplate "html/template" // 별칭 htemplate
-//}
-
-//import { // github.com/matten/go-sqlite3 패키지를 가져왔지만 이 패키지를 직접사용하지 않고 database/sql 패키지의 sqlite3를 사용
-//	"database/sql"
-//	_ "github.com/mattn/go-sqlite3" // _를 이용해서 오류방지
-//}
