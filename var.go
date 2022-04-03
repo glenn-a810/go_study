43c745a1828519346c9085ba438e6dcd2b9d59ca
Glenn <a810.glenn@gmail.com>
2022-04-03 20:06:02 +0900

    ch17 숫자값 입력받기

--- var.go
+++ var.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"math/rand"
-	"time"
+	"os"
 )
 
-func main() {
-	rand.Seed(time.Now().UnixNano()) // 시간 값을 랜덤시드로 설정
+var stdin = bufio.NewReader(os.Stdin)
 
-	n := rand.Intn(100)
-	fmt.Println(n)
+func InputIntValue() (int, error) {
+	var n int
+	_, err := fmt.Scanln(&n) // int 타입값을 입력받기
+	if err != nil {
+		stdin.ReadString('\n') // 에러 발생 시 입력 스트림을 비움
+	}
+	return n, err
+}
+
+func main() {
+	for {
+		fmt.Printf("숫자값을 입력하세요: ")
+		n, err := InputIntValue()
+		if err != nil {
+			fmt.Println("숫자만 입력가능합니다.")
+		} else {
+			fmt.Println("입력하신 숫자는 ", n, " 입니다.")
+		}
+	}
 }
