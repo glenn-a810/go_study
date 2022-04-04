b8735ea6e7a22eefae9492c214cec42d099d268a
Glenn <a810.glenn@gmail.com>
2022-04-04 18:28:11 +0900

    ch18 슬라이스 선언. 초기화 방법

--- var.go
+++ var.go
@@ -12,3 +12,11 @@ func main() {
 	slice[1] = 10 // 패닉 발생
 	fmt.Println(slice)
 }
+
+//var slice1 = []int{1,2,3}
+//var slice2 = []int{1,5:2,10:3} // [1 0 0 0 0 2 0 0 0 0 3]
+//
+//var array = [...]int{1,2,3} // 배열 선언. 길이가 3인 고정 길이 배열
+//var slice = []int{1,2,3} // 슬라이스 선언
+//
+//var slice = make([]int, 3) // 길이 3개짜리 int 슬라이스
