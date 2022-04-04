ef4f5ce6c7ed824faec7d80abe5387d2730d39d0
Glenn <a810.glenn@gmail.com>
2022-04-04 18:33:00 +0900

    ch18 슬라이스 요소 추가

--- var.go
+++ var.go
@@ -3,30 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var slice []int
+	var slice = []int{1, 2, 3}
 
-	if len(slice) == 0 {
-		fmt.Println("slice is empty", slice)
-	}
+	slice2 := append(slice, 4)
 
-	slice[1] = 10 // 패닉 발생
 	fmt.Println(slice)
+	fmt.Println(slice2)
 }
-
-//var slice1 = []int{1,2,3}
-//var slice2 = []int{1,5:2,10:3} // [1 0 0 0 0 2 0 0 0 0 3]
-//
-//var array = [...]int{1,2,3} // 배열 선언. 길이가 3인 고정 길이 배열
-//var slice = []int{1,2,3} // 슬라이스 선언
-//
-//var slice = make([]int, 3) // 길이 3개짜리 int 슬라이스
-
-//var slice = []int{1, 2, 3}
-//
-//for i:=0;i<len(slice);i++{
-//	slice[i]+=10
-//}
-//
-//for i,v:=range slice{
-//	slice[i]=v*2
-//}
