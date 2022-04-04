cd6da971c47f7bd1ab93e880876e905bacb643fd
Glenn <a810.glenn@gmail.com>
2022-04-04 18:23:51 +0900

    ch18 슬라이스 선언. 초기화 하지 않으면 길이가 0

--- var.go
+++ var.go
@@ -1,30 +1,14 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-var stdin = bufio.NewReader(os.Stdin)
-
-func InputIntValue() (int, error) {
-	var n int
-	_, err := fmt.Scanln(&n) // int 타입값을 입력받기
-	if err != nil {
-		stdin.ReadString('\n') // 에러 발생 시 입력 스트림을 비움
-	}
-	return n, err
-}
+import "fmt"
 
 func main() {
-	for {
-		fmt.Printf("숫자값을 입력하세요: ")
-		n, err := InputIntValue()
-		if err != nil {
-			fmt.Println("숫자만 입력가능합니다.")
-		} else {
-			fmt.Println("입력하신 숫자는 ", n, " 입니다.")
-		}
+	var slice []int
+
+	if len(slice) == 0 {
+		fmt.Println("slice is empty", slice)
 	}
+
+	slice[1] = 10 // 패닉 발생
+	fmt.Println(slice)
 }
