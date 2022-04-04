abe6f8df5faf619899a6175acaebe0f4db5189ed
Glenn <a810.glenn@gmail.com>
2022-04-04 18:36:57 +0900

    ch18 슬라이스 요소 추가

--- var.go
+++ var.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var slice = []int{1, 2, 3}
+	var slice []int
 
-	//slice2 := append(slice, 4)
-	slice2 := append(slice, 4, 5, 6, 7, 8) // 여러 값 추가
+	for i := 1; i <= 10; i++ {
+		slice = append(slice, i)
+	}
 
+	slice = append(slice, 11, 12, 13, 14, 15)
 	fmt.Println(slice)
-	fmt.Println(slice2)
 }
