cabd19814ca2ee5ae4e4fbbb991f3a43ea6574ae
Glenn <a810.glenn@gmail.com>
2022-04-05 21:56:04 +0900

    ch18 슬라이스와 배열의 동작 차이

--- var.go
+++ var.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
+func changeArray(array2 [5]int) { // 배열을 받아서 세번째 값 변경
+	array2[2] = 200
+}
+
+func changeSlice(slice2 []int) { // 슬라이스를 받아서 세번째 값 변경
+	slice2[2] = 200
+}
+
 func main() {
-	var slice []int
+	array := [5]int{1, 2, 3, 4, 5}
+	slice := []int{1, 2, 3, 4, 5}
 
-	for i := 1; i <= 10; i++ {
-		slice = append(slice, i)
-	}
+	changeArray(array)
+	changeSlice(slice)
 
-	slice = append(slice, 11, 12, 13, 14, 15)
-	fmt.Println(slice)
+	fmt.Println("array:", array)
+	fmt.Println("slice:", slice)
 }
